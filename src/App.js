c0fd28af3f3fec33666a109a5724b9a9335a733a
Tran Nhu Thao <69711031+trannhuthao1007@users.noreply.github.com>
2021-01-21 16:20:30 +0700

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,50 @@
+"/* eslint-disable */" 
+import logo from './logo.svg';
+import './App.css';
+
+
+function App() {
+  return (
+    <div className="App">
+      <div className="right">
+      <header className="title">Introducing Yourself Selft <br></br> Intoduction with Text Boxes</header>
+      <hr className="hr"></hr>
+      <div className="content">
+        <div className="Text">
+          <p><b>TextHere01</b></p>
+          <p>This slide is 100% editable. Adapt t to  your needs and capture your audiance's attention</p>
+        </div>
+
+        <div className="Text">
+          <p><b>TextHere02</b></p>
+          <p>This slide is 100% editable. Adapt t to  your needs and capture your audiance's attention</p>
+        </div>
+
+        <div className="Text">
+          <p><b>TextHere03</b></p>
+          <p>This slide is 100% editable. Adapt t to  your needs and capture your audiance's attention</p>
+        </div>
+
+        <div className="Text">
+          <p><b>TextHere04</b></p>
+          <p>This slide is 100% editable. Adapt t to  your needs and capture your audiance's attention</p>
+        </div>
+
+      </div>
+      </div>
+
+
+     <div className="left">
+       <div className="profile"> 
+       <img src="" />
+       </div>
+       <p className="intro"> Hello</p>
+       
+       <p className="name">My name is Nhu Thao
+       </p>
+     </div>
+    </div>
+  );
+}
+
+export default App;
